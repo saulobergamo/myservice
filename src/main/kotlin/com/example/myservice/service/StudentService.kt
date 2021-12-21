98a3b2a95763ba99b37760aceecd24f6dc5a1f46
Saulo Bergamo <saulobergamo@hotmail.com>
2021-12-21 12:19:05 -0300

    fix response HTTP status

--- src/main/kotlin/com/example/myservice/service/StudentService.kt
+++ src/main/kotlin/com/example/myservice/service/StudentService.kt
@@ -1,36 +1,44 @@
 package com.example.myservice.service
 
+import com.example.myservice.exception.NotFoundRunTime
 import com.example.myservice.model.StudentModel
 import com.example.myservice.repositories.StudentRepository
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 class StudentService(private val studentRepo: StudentRepository){
 
-    fun createStudent(student: StudentModel): StudentModel {
-        return studentRepo.save(student)
+    fun createStudent(student: StudentModel){
+        studentRepo.save(student)
     }
 
     fun readStudents(course: String): List<StudentModel> {
-        return studentRepo.findByCourse(course)
+        if(studentRepo.existsByCourse(course)){
+            return studentRepo.findByCourse(course)
+        }
+        throw NotFoundRunTime("Course doesn't have any student")
     }
 
     fun readAllStudents():List<StudentModel>{
         return studentRepo.findAll()
     }
 
-    fun updateStudent(id: String, student: StudentModel): StudentModel? {
-        return if(studentRepo.existsById(id)){
+    fun updateStudent(id: String, student: StudentModel) {
+        if(studentRepo.existsById(id)){
             this.studentRepo.save(student)
-        } else null
+        }
+        throw NotFoundRunTime("Student doesn't exist")
     }
 
-    fun deleteStudent(id: String) : ResponseEntity<Unit>{
-        return ResponseEntity.ok(studentRepo.deleteById(id))
+
+    fun deleteStudent(id: String) {
+        if(studentRepo.existsById(id)){
+            return studentRepo.deleteById(id)
+        }
+        throw NotFoundRunTime("Student doesn't exist")
     }
 
-    fun deleteAllStudents(){
+    fun deleteAllStudents() {
         studentRepo.deleteAll()
     }
 
